003fc901720f3c27f8147242d2ce0241d1374eb9
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-09-20 23:55:46 +0200

    add notifications and project list to dashboard

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 grey-text text-darken-3">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Dani</p>
+                    <p className="grey-text">3 de setiempre, 13:00</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 grey-text text-darken-3">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Dani</p>
+                    <p className="grey-text">3 de setiempre, 13:00</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 grey-text text-darken-3">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Dani</p>
+                    <p className="grey-text">3 de setiempre, 13:00</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList;
