a9a9d1e85dd97d54a239f4d16e88b48df945b4ee
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-09-22 18:44:57 +0200

    add sign in and sign up components

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
-class SignIn extends Component {
+class SignUp extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        firstName: '',
+        lastName: ''
     }
 
     handleChange = e => {
@@ -20,29 +22,21 @@ class SignIn extends Component {
     render() {
         return (
             <div>
-                {/* <div className="container">
-                    <form onSubmit={this.handleSubmit} className="white">
-                        <h5 className="grey-text text-darken-3">Sign In</h5>
-                        <div className="input-field">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" onChange={this.handleChange} />
-                        </div>
-                        <div className="input-field">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" onChange={this.handle} />
-                        </div>
-                        <div className="input-field">
-                            <button className="btn blue lighten-1">Login</button>
-                        </div>
-                    </form>
-                </div> */}
                 <div className="container">
                     <div className="row">
                         <div className="col s12 m6 offset-m4">
                             <div className="card">
                                 <div className="card-content">
-                                    <span className="card-title">Log In</span>
+                                    <span className="card-title">Sign Up</span>
                                     <form onSubmit={this.handleSubmit} className="white">
+                                        <div className="input-field">
+                                            <label htmlFor="firstName">Name</label>
+                                            <input type="text" name="firstName" id="firstName" onChange={this.handleChange} />
+                                        </div>
+                                        <div className="input-field">
+                                            <label htmlFor="lastName">Last Name</label>
+                                            <input type="text" name="lastName" id="lastName" onChange={this.handleChange} />
+                                        </div>
                                         <div className="input-field">
                                             <label htmlFor="email">Email</label>
                                             <input type="email" name="email" id="email" onChange={this.handleChange} />
@@ -52,7 +46,7 @@ class SignIn extends Component {
                                             <input type="password" name="password" id="password" onChange={this.handleChange} />
                                         </div>
                                         <div className="input-field">
-                                            <button className="btn blue lighten-1 waves-effect waves-large">Login</button>
+                                            <button className="btn blue lighten-1 waves-effect waves-large">Sign Up</button>
                                         </div>
                                     </form>
                                 </div>
@@ -65,4 +59,4 @@ class SignIn extends Component {
     }
 }
 
-export default SignIn
+export default SignUp
