216d3cc8a3fc1155401a591a40d7f2cf1a7f43fd
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-09-27 16:22:44 +0200

    add createproject form component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetail from './components/projects/ProjectDetail';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route exact path='/' component={Dashboard} />
+            <Route exact path='/create' component={CreateProject} />
             <Route path='/project/:id' component={ProjectDetail} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
