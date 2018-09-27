5df73162ef814c63c6e6497cc48bb4efc67ec683
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-09-27 16:14:51 +0200

    change materialize style of signin and signup  forms

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -19,47 +19,21 @@ class SignIn extends Component {
 
     render() {
         return (
-            <div>
-                {/* <div className="container">
-                    <form onSubmit={this.handleSubmit} className="white">
-                        <h5 className="grey-text text-darken-3">Sign In</h5>
-                        <div className="input-field">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" onChange={this.handleChange} />
-                        </div>
-                        <div className="input-field">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" onChange={this.handle} />
-                        </div>
-                        <div className="input-field">
-                            <button className="btn blue lighten-1">Login</button>
-                        </div>
-                    </form>
-                </div> */}
-                <div className="container">
-                    <div className="row">
-                        <div className="col s12 m6 offset-m4">
-                            <div className="card">
-                                <div className="card-content">
-                                    <span className="card-title">Log In</span>
-                                    <form onSubmit={this.handleSubmit} className="white">
-                                        <div className="input-field">
-                                            <label htmlFor="email">Email</label>
-                                            <input type="email" name="email" id="email" onChange={this.handleChange} />
-                                        </div>
-                                        <div className="input-field">
-                                            <label htmlFor="password">Password</label>
-                                            <input type="password" name="password" id="password" onChange={this.handleChange} />
-                                        </div>
-                                        <div className="input-field">
-                                            <button className="btn blue lighten-1 waves-effect waves-large">Login</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
+            <div className="container">
+                <form className="white" onSubmit={this.handleSubmit}>
+                    <h5 className="grey-text text-darken-3">Sign In</h5>
+                    <div className="input-field">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id='email' onChange={this.handleChange} />
                     </div>
-                </div>
+                    <div className="input-field">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id='password' onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <button className="btn blue lighten-1 z-depth-0">Login</button>
+                    </div>
+                </form>
             </div>
         )
     }
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -21,39 +21,29 @@ class SignUp extends Component {
 
     render() {
         return (
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col s12 m6 offset-m4">
-                            <div className="card">
-                                <div className="card-content">
-                                    <span className="card-title">Sign Up</span>
-                                    <form onSubmit={this.handleSubmit} className="white">
-                                        <div className="input-field">
-                                            <label htmlFor="firstName">Name</label>
-                                            <input type="text" name="firstName" id="firstName" onChange={this.handleChange} />
-                                        </div>
-                                        <div className="input-field">
-                                            <label htmlFor="lastName">Last Name</label>
-                                            <input type="text" name="lastName" id="lastName" onChange={this.handleChange} />
-                                        </div>
-                                        <div className="input-field">
-                                            <label htmlFor="email">Email</label>
-                                            <input type="email" name="email" id="email" onChange={this.handleChange} />
-                                        </div>
-                                        <div className="input-field">
-                                            <label htmlFor="password">Password</label>
-                                            <input type="password" name="password" id="password" onChange={this.handleChange} />
-                                        </div>
-                                        <div className="input-field">
-                                            <button className="btn blue lighten-1 waves-effect waves-large">Sign Up</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="container">
+                <form className="white" onSubmit={this.handleSubmit}>
+                <h5 className="grey-text text-darken-3">Sign Up</h5>
+                <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' onChange={this.handleChange} />
                 </div>
+                <div className="input-field">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id='password' onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id='firstName' onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id='lastName' onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                    <button className="btn blue lighten-1 z-depth-0">Sign Up</button>
+                </div>
+                </form>
             </div>
         )
     }
