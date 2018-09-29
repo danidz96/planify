1366319a46c0f2705a881b215d00dd2f786738ac
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-09-29 13:37:21 +0200

    add styles to the forms

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -20,20 +20,22 @@ class SignIn extends Component {
     render() {
         return (
             <div className="container">
-                <form className="white" onSubmit={this.handleSubmit}>
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-                    <div className="input-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id='email' onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id='password' onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <button className="btn blue lighten-1 z-depth-0">Login</button>
-                    </div>
-                </form>
+                <div className="row">
+                    <form className="white col s12 l6 offset-l3" onSubmit={this.handleSubmit}>
+                        <h5 className="grey-text text-darken-3">Sign In</h5>
+                        <div className="input-field">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id='email' onChange={this.handleChange} />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id='password' onChange={this.handleChange} />
+                        </div>
+                        <div className="input-field">
+                            <button className="btn blue lighten-1 z-depth-0">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -22,28 +22,30 @@ class SignUp extends Component {
     render() {
         return (
             <div className="container">
-                <form className="white" onSubmit={this.handleSubmit}>
-                <h5 className="grey-text text-darken-3">Sign Up</h5>
-                <div className="input-field">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id='email' onChange={this.handleChange} />
+                <div className="row">
+                    <form className="white col s12 l6 offset-l3" onSubmit={this.handleSubmit}>
+                        <h5 className="grey-text text-darken-3">Sign Up</h5>
+                        <div className="input-field">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id='email' onChange={this.handleChange} />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id='password' onChange={this.handleChange} />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="firstName">First Name</label>
+                            <input type="text" id='firstName' onChange={this.handleChange} />
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="lastName">Last Name</label>
+                            <input type="text" id='lastName' onChange={this.handleChange} />
+                        </div>
+                        <div className="input-field">
+                            <button className="btn blue lighten-1 z-depth-0">Sign Up</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="input-field">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id='password' onChange={this.handleChange} />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="firstName">First Name</label>
-                    <input type="text" id='firstName' onChange={this.handleChange} />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="lastName">Last Name</label>
-                    <input type="text" id='lastName' onChange={this.handleChange} />
-                </div>
-                <div className="input-field">
-                    <button className="btn blue lighten-1 z-depth-0">Sign Up</button>
-                </div>
-                </form>
             </div>
         )
     }
