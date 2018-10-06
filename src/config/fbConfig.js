d34ad0724b8f19cf8697fd3a944f72188ea53a98
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-10-06 11:28:17 +0200

    add firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBqPrSgwIGgSvQ2Ap7JikZsT5qXcc8cYDM",
+    authDomain: "planify-5fdd8.firebaseapp.com",
+    databaseURL: "https://planify-5fdd8.firebaseio.com",
+    projectId: "planify-5fdd8",
+    storageBucket: "planify-5fdd8.appspot.com",
+    messagingSenderId: "930536533949"
+  };
+  firebase.initializeApp(config);
+  firebase.firebase().settings({ timestampsInSnapshots: true });
+
+  export default firebase;
