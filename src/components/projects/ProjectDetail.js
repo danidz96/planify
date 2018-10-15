abe7157368b77b525fdbe6bdb87b499ca605417a
Dani Domínguez <dani.d.zamorano@gmail.com>
2018-10-15 15:34:58 +0200

    add route guards

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux'
+import { compose } from 'redux';
+import { Redirect } from 'react-router-dom'
 
 const ProjectDetail = (props) => {
-    const { project } = props;
+    const { project, auth } = props;
+    if (!auth.uid) return <Redirect to='/signin' />
+
     if (project) {
        return (
             <div>
@@ -37,7 +40,8 @@ const mapStateToProps = (state, ownProps) => {
     const projects = state.firestore.data.projects;
     const project = projects ? projects[id] : null;
     return{
-        project: project
+        project: project,
+        auth: state.firebase.auth
     }
 }
 
