5c552eeaa2af4a06f5e48619486101cc4548009a
danidz96 <dani.d.zamorano@gmail.com>
2018-10-18 21:23:06 +0200

    add author info when create new project

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({project}) => {
         <div className="card z-depth-0 grey-text text-darken-3">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{project.title}</span>
-                <p>Posted by Dani</p>
+                <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
                 <p className="grey-text">3 de septiembre, 13:00</p>
             </div>
         </div>
