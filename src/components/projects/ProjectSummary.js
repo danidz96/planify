5af530520de2347ee5658ec55bc9566e8d47264c
danidz96 <dani.d.zamorano@gmail.com>
2018-10-24 20:23:20 +0200

    modify card style

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 
 const ProjectSummary = ({project}) => {
     return(
-        <div className="card z-depth-0 grey-text text-darken-3">
+        <div className="card grey-text text-darken-3">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{project.title}</span>
                 <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
