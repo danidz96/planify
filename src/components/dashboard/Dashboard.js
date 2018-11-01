52c50adcb9583ccc7b42a0653a66015621f58fc2
danidz96 <dani.d.zamorano@gmail.com>
2018-11-01 14:01:31 +0100

    show notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -10,7 +10,7 @@ import { Redirect } from 'react-router-dom'
 class Dashboard extends Component {
     render(){
         // console.log(this.props);
-        const { projects, auth } = this.props;
+        const { projects, auth, notifications } = this.props;
         if (!auth.uid) return <Redirect to='/signin' />
         return(
             <div className="dashboard container">
@@ -19,7 +19,7 @@ class Dashboard extends Component {
                         <ProjectList projects={projects}/>    
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications />
+                        <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -30,13 +30,15 @@ class Dashboard extends Component {
 const mapStateToProps = state => {
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects'}
+        { collection: 'projects' },
+        { collection: 'notifications', limit: 5 }
     ])
 )(Dashboard);
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 const Notifications = (props) => {
+    const {notifications} = props;
     return (
         <div className="section">
             <div className="card">
                 <div className="card-content">
                     <span className="card-title">Notifications</span>
                     <ul className="notifications">
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
+                        { notifications && notifications.map(item => {
+                            return (
+                                <li key={item.id}>
+                                    <span className="blue-text">{item.user}</span>
+                                    <span>  {item.content}</span>
+                                    <div className="grey-text note-date">
+                                        {moment(item.time.toDate()).fromNow()}
+                                    </div>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
