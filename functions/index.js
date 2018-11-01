9f790674f60705aec38ea9be04ec4e8159dc490b
danidz96 <dani.d.zamorano@gmail.com>
2018-11-01 13:46:44 +0100

    add cloud function that triggers when  anyone signs up and add a notification

--- functions/index.js
+++ functions/index.js
@@ -25,4 +25,19 @@ exports.projectCreated = functions.firestore
         }
 
         return createNotification(notification);
-})
+});
+
+exports.userJoined = functions.auth.user()
+    .onCreate(user => {
+        return admin.firestore().collection('users')
+            .doc(user.uid).get().then(doc => {
+                const newUser = doc.data();
+                const notification = {
+                    constent: 'Joined the app',
+                    user: `${newUser.firstName} ${newUser.lastName}`,
+                    time: admin.firestore.FieldValue.serverTimestamp()
+                }
+
+                return createNotification(notification);
+            })
+    })
